087d3d3704c770320361013d410004e49681227b
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2018-01-08 04:22:43 +0100

    -add tests

---
+++ src/test/java/GUI/ApplicationGUITest.java
@@ -0,0 +1,24 @@
+package GUI;
+
+import Database.DatabaseConnection;
+import GUI.Overviews.Workers;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+
+public class ApplicationGUITest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        LoginDialog loginDialog = new LoginDialog();
+        Connection connection = new DatabaseConnection(loginDialog.getUsername(),loginDialog.getPassword()).getConnection();
+        primaryStage.setScene(new Scene(new Workers(primaryStage,connection)));
+        primaryStage.show();
+    }
+}
